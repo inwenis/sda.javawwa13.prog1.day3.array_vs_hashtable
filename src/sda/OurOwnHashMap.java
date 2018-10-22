1ccd935bb4627ac85acd0191edea2bc05d266aa7
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-22 08:22:35 +0200

    implement hash map

--- src/sda/OurOwnHashMap.java
+++ src/sda/OurOwnHashMap.java
@@ -6,10 +6,23 @@ import java.util.stream.Collectors;
 public class OurOwnHashMap implements Map<String, Patient> {
 
     private ArrayList<ArrayList<Patient>> patients;
+    private final int capacity;
 
-    // TODO this is the place where you have to work
-    // it's probably best to start with the `put()` method
-    // for simple tests you can use `OutOwnHashMapTests.main()`
+    public OurOwnHashMap() {
+        capacity = 10000000;
+        patients = new ArrayList<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            patients.add(null);
+        }
+    }
+
+    public OurOwnHashMap(int capacity) {
+        this.capacity = capacity;
+        patients = new ArrayList<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            patients.add(null);
+        }
+    }
 
     @Override
     public int size() {
@@ -23,8 +36,16 @@ public class OurOwnHashMap implements Map<String, Patient> {
 
     @Override
     public boolean containsKey(Object key) {
-        // TODO implement
-        return false;
+        int hash = hash((String) key);
+        ArrayList<Patient> list = patients.get(hash);
+        if(list == null) {
+            return false;
+        } else {
+            boolean patientFound = list
+                    .stream()
+                    .anyMatch(x -> x.Pesel.equals(key));
+            return patientFound;
+        }
     }
 
     @Override
@@ -34,19 +55,43 @@ public class OurOwnHashMap implements Map<String, Patient> {
 
     @Override
     public Patient get(Object key) {
-        // TODO implement
-        return null;
+        int hash = hash((String) key);
+        ArrayList<Patient> list = patients.get(hash);
+        if(list == null) {
+            return null;
+        }
+        else {
+            List<Patient> patientsFound = list
+                    .stream()
+                    .filter(x -> x.Pesel.equals(key))
+                    .collect(Collectors.toList());
+            if (patientsFound.size() > 1) {
+                throw new RuntimeException("This should not happen");
+            }
+            return patientsFound.get(0);
+        }
     }
 
     private int hash(String s) {
-        // TODO implement
-        return 0;
+        int hash = 7;
+        for (int i = 0; i < s.length(); i++) {
+            hash = hash*31 + s.charAt(i);
+        }
+        hash = Math.abs(hash);
+        hash = hash % capacity;
+        return hash;
     }
 
     @Override
     public Patient put(String key, Patient value) {
-        // TODO implement
-        return null;
+        int hash = hash(key);
+        ArrayList<Patient> list = patients.get(hash);
+        if(list == null) {
+            patients.set(hash, new ArrayList<>());
+            list = patients.get(hash);
+        }
+        list.add(value);
+        return value;
     }
 
     @Override
--- src/sda/OurOwnHashMapTests.java
+++ src/sda/OurOwnHashMapTests.java
@@ -1,10 +1,19 @@
 package sda;
 
+import java.util.Map;
+
 public class OurOwnHashMapTests {
     public static void main(String[] args) {
-        // create hash map
-        // Map<String, Patient> map = new OurOwnHashMap();
+        Map<String, Patient> map = new OurOwnHashMap(10);
 
-        // do some simple tests here
+        Patient patient1 = new Patient();
+        patient1.Name = "Joe Doe";
+        patient1.Pesel = "80101012123";
+        map.put(patient1.Pesel, patient1);
+
+        System.out.println("expected: true actual: " + map.containsKey(patient1.Pesel));
+        System.out.println("expected: false actual: " + map.containsKey("67676767676"));
+        Patient patientFromMap = map.get(patient1.Pesel);
+        System.out.println("expected: true actual: " + (patient1 == patientFromMap));
     }
 }
